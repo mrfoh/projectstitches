745ed1253adfd621d12d95481301a4b4501453cd
Patrick Foh <patrickfoh@gmail.com>
2016-02-11 08:47:04 +0100

    Added migration

---
+++ database/migrations/2016_02_11_074402_update_transaction_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateTransactionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('transactions', function($table) {
+            $table->double('amount', 10, 2)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('transactions');
+    }
+}
